eeaa42adf8063a09fce3c735923fafe4be6966ee
Demo User <debian@beaglebone.localdomain>
2019-11-05 03:06:01 +0000

    cleaned up

--- controller_rumble.py
+++ controller_rumble.py
@@ -1,13 +1,8 @@
 import subprocess
 
-#   rumble [ joystick (1-8) ]  [strong motor magnitude (%) ]  [weak motor magnitude (%) ]  [duration (miliseconds) ]
-
-# output = subprocess.run(['./rumble', '0', '100', '100', '1000'], stdout=subprocess.PIPE)
-# print(output.returncode)
-
 def rumble(weak_motor, strong_motor, duration, joystick=0):
     c_rumble = subprocess.run(['./rumble', str(joystick), str(strong_motor), str(weak_motor), str(duration)], stdout=subprocess.PIPE)
-    # print(c_rumble)
+
     if c_rumble.returncode == 0:
         pass
     elif c_rumble.returncode == 1:
@@ -19,4 +14,4 @@ def rumble(weak_motor, strong_motor, duration, joystick=0):
     elif c_rumble.returncode == 4:
         print("Rumble requires sudo!")
 
-    return
+    return c_rumble.returncode
